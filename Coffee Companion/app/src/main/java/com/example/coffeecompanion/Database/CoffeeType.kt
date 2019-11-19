9eb3faf80be3c55ad2b17ff59315bc7d981ca39c
Curtisww17 <wcvikingdude@gmail.com>
2019-11-18 19:27:24 -0500

    Added Main Database Functionality
    Created the coffee types data type and the coffee types table, allowing for insertion and selection from it

---
+++ Coffee Companion/app/src/main/java/com/example/coffeecompanion/Database/CoffeeType.kt
@@ -0,0 +1,26 @@
+package com.example.coffeecompanion.Database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "coffee_types_table")
+data class CoffeeType (
+    @PrimaryKey(autoGenerate = true)
+    var coffeeId: Long = 0L,
+
+    @ColumnInfo(name = "name")
+    val name: String = "",
+
+    @ColumnInfo(name = "mins_to_brew")
+    val minsToBrew: Double = 0.0,
+
+    @ColumnInfo(name = "grind")
+    val grind: String = "",
+
+    @ColumnInfo(name = "instructions")
+    val instructions: String = "",
+
+    @ColumnInfo(name = "amount")
+    val amount: Double = 0.0
+    )
