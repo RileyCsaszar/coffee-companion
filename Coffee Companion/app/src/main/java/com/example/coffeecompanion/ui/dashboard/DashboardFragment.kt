62a8d7562326785800b64e5686304103a2e1ef3d
GCC\MCKINNISJP2 <mckinnisjp2@gcc.edu>
2019-12-17 12:48:36 -0500

    Fixed bugs with quiz view

--- Coffee Companion/app/src/main/java/com/example/coffeecompanion/ui/dashboard/DashboardFragment.kt
+++ Coffee Companion/app/src/main/java/com/example/coffeecompanion/ui/dashboard/DashboardFragment.kt
@@ -26,20 +26,20 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
             ViewModelProviders.of(this).get(DashboardViewModel::class.java)
 
-        binding.q1Option1.setOnClickListener { update(1, 1) }
-        binding.q1Option2.setOnClickListener { update(1, 2) }
-        binding.q2Option1.setOnClickListener { update(2, 1) }
-        binding.q2Option2.setOnClickListener { update(2, 2) }
-        binding.q3Option1.setOnClickListener { update(3, 1) }
-        binding.q3Option2.setOnClickListener { update(3, 2) }
-        binding.q4Option1.setOnClickListener { update(4, 1) }
-        binding.q4Option2.setOnClickListener { update(4, 2) }
-        binding.q5Option1.setOnClickListener { update(5, 1) }
-        binding.q5Option2.setOnClickListener { update(5, 2) }
-        binding.q6Option1.setOnClickListener { update(6, 1) }
-        binding.q6Option2.setOnClickListener { update(6, 2) }
-        binding.q7Option1.setOnClickListener { update(7, 1) }
-        binding.q7Option2.setOnClickListener { update(7, 2) }
+        binding.q1Option1.setOnClickListener { update(0, 1) }
+        binding.q1Option2.setOnClickListener { update(0, 2) }
+        binding.q2Option1.setOnClickListener { update(1, 1) }
+        binding.q2Option2.setOnClickListener { update(1, 2) }
+        binding.q3Option1.setOnClickListener { update(2, 1) }
+        binding.q3Option2.setOnClickListener { update(2, 2) }
+        binding.q4Option1.setOnClickListener { update(3, 1) }
+        binding.q4Option2.setOnClickListener { update(3, 2) }
+        binding.q5Option1.setOnClickListener { update(4, 1) }
+        binding.q5Option2.setOnClickListener { update(4, 2) }
+        binding.q6Option1.setOnClickListener { update(5, 1) }
+        binding.q6Option2.setOnClickListener { update(5, 2) }
+        binding.q7Option1.setOnClickListener { update(6, 1) }
+        binding.q7Option2.setOnClickListener { update(6, 2) }
 
         binding.submitButton.setOnClickListener {
             dashboardViewModel.calcResult()
